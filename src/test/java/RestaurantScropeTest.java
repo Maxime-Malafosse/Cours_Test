59eb42d0e4d82cddae3c5c888ab52f0b7a43efb0
malafossem <maxime.malafosse@etu.umontpellier.fr>
2022-01-05 16:18:22 +0100

    Scope Restaurant fait.

---
+++ src/test/java/RestaurantScropeTest.java
@@ -0,0 +1,67 @@
+import Utilities.FranchiseBuilder;
+import Utilities.RestaurantBuilder;
+import org.junit.Test;
+import Restaurant.*;
+
+import java.util.ArrayList;
+
+import static org.junit.Assert.assertEquals;
+
+
+public class RestaurantScropeTest {
+    @Test
+    public void Restaurant_Avec_0_Serveur(){
+
+        //Etant donné un restaurant avec 2 serveurs
+        Restaurant r = new RestaurantBuilder().build();
+
+
+        assertEquals(0, r.chiffreAffaires(),0);
+    }
+    @Test
+    public void Restaurant_Avec_Un_Serveur(){
+
+        //Etant donné un restaurant avec 2 serveurs
+        Restaurant r = new RestaurantBuilder().ayantXServeurs(1).build();
+
+
+        //Quand chacun prend une commande
+        double montantCommande = 1.0;
+        for(Serveur s : r.getListServeurs()){
+            s.prendreCommande(montantCommande);
+        }
+
+        assertEquals(montantCommande, r.chiffreAffaires(),0);
+    }
+
+    @Test
+    public void Restaurant_Avec_Deux_Serveur(){
+
+        //Etant donné un restaurant avec 2 serveurs
+        Restaurant r = new RestaurantBuilder().ayantXServeurs(2).build();
+
+
+        //Quand chacun prend une commande
+        double montantCommande = 1.0;
+        for(Serveur s : r.getListServeurs()){
+            s.prendreCommande(montantCommande);
+        }
+
+        assertEquals(montantCommande + montantCommande, r.chiffreAffaires(),0);
+    }
+
+    @Test
+    public void Restaurant_Avec_Cent_Serveurs(){
+        //Etant donné un restaurant avec 2 serveurs
+        Restaurant r = new RestaurantBuilder().ayantXServeurs(100).build();
+
+
+        //Quand chacun prend une commande
+        double montantCommande = 1.0;
+        for(Serveur s : r.getListServeurs()){
+            s.prendreCommande(montantCommande);
+        }
+
+        assertEquals(100 * 1.0, r.chiffreAffaires(),0.1);
+    }
+}
